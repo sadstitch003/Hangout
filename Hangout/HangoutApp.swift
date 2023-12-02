383dcb6bcd7924a7451ec03681884b6310767638
sadstitch003 <80204631+sadstitch003@users.noreply.github.com>
2023-12-03 06:48:56 +0700

    Add files via upload

---
+++ Hangout/HangoutApp.swift
@@ -0,0 +1,17 @@
+//
+//  HangoutApp.swift
+//  Hangout
+//
+//  Created by MacBook Pro on 03/12/23.
+//
+
+import SwiftUI
+
+@main
+struct HangoutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
