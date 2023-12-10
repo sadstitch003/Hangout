95936bfb1795297c4ae5502bae4ffadec775ba88
Nathan3122000 <95455078+Nathan3122000@users.noreply.github.com>
2023-12-10 20:41:53 +0700

    Update LoginFormView.swift

--- Hangout/LoginFormView.swift
+++ Hangout/LoginFormView.swift
@@ -8,14 +8,49 @@
 import SwiftUI
 
 struct LoginFormView: View {
+    @State private var username = ""
+    @State private var password = ""
+
     var body: some View {
-        NavigationView {
+        VStack {
             Text("Login Form")
+                .font(.title)
+                .padding()
+
+            TextField("Username", text: $username)
+                .padding()
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+
+            SecureField("Password", text: $password)
+                .padding()
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+
+            Button(action: {
+                // Add login functionality here
+                print("Login tapped")
+            }) {
+                Text("Login")
+                    .padding()
+                    .foregroundColor(.white)
+                    .background(Color.blue)
+                    .cornerRadius(10)
+            }
+            .padding()
+
+            Spacer()
         }
+        .padding()
         .navigationBarBackButtonHidden(true)
     }
-    
 }
+
+#if DEBUG
+struct LoginForm_Previews: PreviewProvider {
+    static var previews: some View {
+        LoginFormView()
+    }
+}
+#endif
 
 #Preview {
     LoginFormView()
